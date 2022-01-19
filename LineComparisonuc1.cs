5f3ebb980db46676a3ab1e67208b4d14028a772d
nilanjana20 <nilanjanac873@gmail.com>
2022-01-19 21:10:39 +0530

    Line comparison program added

---
+++ LineComparisonuc1.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Assignments;
+internal class LineComparison
+{
+    public static double LengthOfLine(int x1, int y1, int x2, int y2)
+    {
+        return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    }
+    public static void Solution()
+    {
+        Console.Write("x1 = ");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("y1 = ");
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("x2 = ");
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("y2 = ");
+        int y2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"length of line is {LengthOfLine(x1, y1, x2, y2)} unit");
+
+        Console.WriteLine("Welcome to Line Comparison Computation Program");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,16 +4,20 @@ namespace Assignments
 {
 	public class Program
 	{
-		public static void Main()
-			{
-				// EmployeeWage.AttendanceCheck();
+				public static void Main()
+			{   
+				//Employeewage Program
+                // EmployeeWage.AttendanceCheck();
 				// Console.WriteLine("Welcome to Employeewage computation program on master branch");
 			    // Console.WriteLine(Assignments.EmployeeWage.AttendanceCheck()? "Employee is present" : "Employee is absent");
 				//  Assignments.EmployeeWageCaculation res = new Assignments.EmployeeWageCaculation();
                 //  Console.WriteLine(res.dailyEmployeeWage());
+				//  Assignments.EmployeeWageSwitchCase.Solution();
 
-				 Assignments.EmployeeWageSwitchCase.Solution();
-			}
-	}
-}
+				//Line-comparison
+                Assignments.LineComparison.Solution();
+            }
+    }
+}               
+
 
